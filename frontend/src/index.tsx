5f5664daff31b1128db1adf7e5819221133a1b1b
sajalkhan <sajalhossen@yahoo.com>
2023-10-15 12:58:20 +0600

    update style

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,7 +2,6 @@ import { ConfigProvider } from 'antd';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-import 'antd/dist/antd.css';
 import App from 'pages/App';
 import { customTheme } from './Theme';
 import './styles/index.scss';
